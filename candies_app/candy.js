41bb960421dd6476bf635af5160d4f8abac000ec
Philippa Johnstone <pecj@hotmail.co.uk>
2015-11-11 07:13:03 +0800

    create, update and delete not working

---
+++ candies_app/candy.js
@@ -0,0 +1,24 @@
+var candy = [
+  {
+    "id": 1,
+    "name": "chewing gum",
+    "color": "red"
+  },
+  {
+    "id": 2,
+    "name": "pez",
+    "color": "green"
+  },
+  {
+    "id": 3,
+    "name": "marshmallow",
+    "color": "pink"
+  },
+  {
+    "id": 4,
+    "name": "candy stick",
+    "color": "blue"
+  }
+]
+
+module.exports = candy;
