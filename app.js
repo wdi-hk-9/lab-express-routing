2b159b72ef547e63be1117e3de68b6dd3d0b82b7
Bryan Fong <bryan.fch@gmail.com>
2015-11-11 00:16:14 +0800

    index and show done

---
+++ app.js
@@ -0,0 +1,40 @@
+// Initialization
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+var candies = require('./candy');
+
+//Routes
+var candyRouter = express.Router();
+
+//Index
+candyRouter.get('/', function(req, res){
+  res.json(candies);
+});
+
+//Show
+candyRouter.get('/:id', function(req, res){
+  var candy = candies.getCandyById(req.params.id)
+  res.json(candy);
+});
+
+//Create
+candyRouter.post('/', function(req, res){
+  res.json("This is Create");
+});
+
+//Update
+candyRouter.put('/:id', function(req, res){
+  res.json("This is Update");
+});
+
+//delete
+candyRouter.delete('/:id', function(req, res){
+  res.json("This is Delete");
+});
+
+app.use('/candies', candyRouter) ///now it turns router become a middleware
+
+// Server Started
+app.listen(port);
+console.log('Server started');
---
+++ candy.js
@@ -0,0 +1,12 @@
+var candies = [
+  {"id":1,"name":"Chewing Gum","color":"Red"},
+  {"id":2,"name":"Pez","color":"Green"},{"id":3,"name":"Marshmallow","color":"Pink"},
+  {"id":4,"name":"Candy Stick","color":"Blue"}
+  ]
+
+module.exports.candy = candies;
+module.exports.getCandyById = function(id){
+  for(var i=0; i<candies.length; i++){
+    if (candies[i].id == id) return candies[i];
+  }
+}
