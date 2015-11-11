3529f25983878b1cf42b968034b0f1cbdff48fec
Toni Anne Romero <tonianne.romero@gmail.com>
2015-11-11 08:40:30 +0800

    done working in curl

--- app.js
+++ app.js
@@ -1,26 +1,51 @@
-var candies = require('./candies')
+var mycandies = require('./candies')
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 var candiesRouter = express.Router();
+var bodyParser = require('body-parser');
 
 app.use(function(req, res, next){
   console.log('%s request to %s from %s', req.method, req.path, req.ip);
   next();
 });
+app.use(bodyParser.json());
 
+
+// INDEX
 candiesRouter.get('/', function(req, res){
-  // res.send('this is the INDEX');
-  res.json({candies});
+  res.json(mycandies.getCandies);
 });
 
-candiesRouter.get('/:id', function(req, res) {
-  // var id = candies[req.params.id];
-  res.json(req.params.id);
+// SHOW
+candiesRouter.get('/:id', mycandies.findById);
+  /* 2nd version
+    candiesRouter.get('/:id', function(req, res) {
+      var id = candies.getCandyById(req.params.id);
+      res.status(200).json(id);
+    });
+  */
+
+// CREATE
+candiesRouter.post('/', mycandies.addCandies);
+
+// UPDATE
+candiesRouter.put('/:id', function(req, res) {
+  var candy = mycandies.getCandies[req.params.id]
+  candy.id = mycandies.getCandies + 1
+  candy.name = req.body.name;
+  candy.color = req.body.color;
+  res.json(candy);
+});
+
+// DELETE
+candiesRouter.delete('/:id', function(req, res) {
+  mycandies.getCandies.splice(req.params.id-1, 1)
+  res.json({"message":"deleted"})
 });
 
 app.use('/candies', candiesRouter);
 
 app.listen(port);
 console.log('Sweet Server is running on ' + port);
-// console.log(candies);
+console.log(mycandies.getCandies);
--- candies.js
+++ candies.js
@@ -11,5 +11,29 @@ var candies = [ { "id":1,
                   "name":"Candy Stick",
                   "color":"Blue"}]
 
+module.exports.getCandies = candies;
+
+module.exports.addCandies = function(req, res){
+  var newCandy = {
+    "name" : req.body.name,
+    "color" : req.body.color
+  };
+  candies.push(newCandy);
+  res.send(req.body);
+};
+
+module.exports.findById = function(req, res) {
+  res.send({id:req.params.id, name: "The Name", color: "The color"});
+};
+
+/* 2nd version to get ID (looping)
+module.exports.getCandyById = function(id) {
+  for (var key in candies) {
+    if (candies[key].id == id)
+      return candies[key];
+  }
+};
+*/
+
 module.exports.candies = candies;
 console.log("module CANDIES initialized")
